27ab1f5c6ca45cd59537501d9109d4f0087014ce
keila ayelen lima <keila_alima@hotmail.com>
2023-03-06 21:36:22 -0300

    editando 3er entrega

---
+++ main.js
@@ -0,0 +1,130 @@
+// Mis contenedores
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modalContainer");
+
+
+// Mis productos 
+const productos = [
+    {
+        id: 1,
+        nombre: "Tomate",
+        precio: 300,
+        img: "https://th.bing.com/th/id/OIP.jP2VYVGUy-fX0djlFDZGfAHaE6?pid=ImgDet&rs=1"
+    },
+    {
+        id: 2,
+        nombre: "Cebolla",
+        precio: 450,
+        img: "https://greens.com.gt/wp-content/uploads/2020/04/Cebolla-Blanca.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Morron",
+        precio: 500,
+        img: "https://th.bing.com/th/id/R.9a8f44ccd90741895d112201db2d5119?rik=5NnavbO5sH2Q1Q&riu=http%3a%2f%2flahoradelasalud.com%2fblog%2fwp-content%2fuploads%2f2020%2f11%2fMORRON-2-1024x768.jpg&ehk=ti%2bEqGyc%2foxEwce%2fdAw71pxsmzSs1FZq%2f8Gd%2fJR%2f%2faY%3d&risl=&pid=ImgRaw&r=0"
+    },
+    {
+        id: 4,
+        nombre: "Papa",
+        precio: 200,
+        img: "https://www.todoparaellas.com/u/fotografias/m/2021/3/5/f800x450-18927_70373_5050.jpg"
+    },
+    {
+        id: 5,
+        nombre: "Ajo",
+        precio: 100,
+        img: "https://cdn.cienradios.com/wp-content/uploads/sites/3/2020/09/ajo.jpg"
+    },
+    {
+        id: 6,
+        nombre: "Manzana",
+        precio: 280,
+        img: "https://th.bing.com/th/id/OIP.YdBKMKhZk429atkjPqNh1AHaHa?pid=ImgDet&rs=1"
+    },
+    {
+        id: 7,
+        nombre: "Brocoli",
+        precio: 670,
+        img: "https://misremedios.com/wp-content/uploads/2016/09/brocoli.jpg"
+    },
+    {
+        id: 8,
+        nombre: "Hongos",
+        precio: 170,
+        img: "https://th.bing.com/th/id/OIP.VlSO5XMwrnMHYs5qQ9JOfgHaE7?pid=ImgDet&rs=1"
+    }
+]
+
+// [   ]
+// <  >
+//Ver carrito
+
+let carrito = [];
+productos.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+    <img src= "${product.img}">
+    <h3> ${product.nombre}</h3>
+    <p class="price">${product.precio}</p>
+    `;
+
+    shopContent.append(content);
+
+    let comprar = document.createElement("button");
+    comprar.innerText = "Comprar";
+    comprar.className = "comprar";
+    content.append(comprar);
+
+    comprar.addEventListener("click", () => {
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+        });
+    });
+});
+
+//Eventos 
+verCarrito.addEventListener("click", () => {
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class="modalHeaderTitle">Carrito</h1>
+    `;
+
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h2");
+    modalbutton.innerText = "x";
+    modalbutton.className = "modalHeaderButton";
+
+    modalbutton.addEventListener ("click", () => {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "modalContent";
+        carritoContent.innerHTML = `
+        <img scr="${product.img}">
+        <h3> ${product.nombre}</h3>
+        <p>$${product.precio}</p>
+    `;
+    modalContainer.append (carritoContent);
+    });
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+    const totalFinal = document.createElement ("div");
+    totalFinal.className = "totalFinal";
+    totalFinal.innerHTML = `El total a pagar es $${total}`;
+    modalContainer.append(totalFinal);
+});
+
+
+
